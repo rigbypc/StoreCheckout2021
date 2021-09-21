8b487815259a97e88278cf437164284c7b786a19
Peter  C Rigby <rigbypc@gmail.com>
2021-09-21 12:46:31 -0400

    Started the adapter pattern

---
+++ src/point/of/sale/ArtR56Display.java
@@ -0,0 +1,13 @@
+package point.of.sale;
+
+public class ArtR56Display {
+	ArtR56 artR56;
+	
+	public ArtR56Display() {
+		artR56 = new ArtR56;
+	}
+	
+	public void showLine(String barcode) {
+		artR56.showLine(barcode);
+	}
+}
