346a9c5230ee126c6ddfcea8ec7767098bf840de
Peter  C Rigby <rigbypc@gmail.com>
2021-09-21 10:58:43 -0400

    starting point for point of sale

---
+++ src/point/of/sale/test/TestSale.java
@@ -0,0 +1,21 @@
+package point.of.sale.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import point.of.sale.*;
+
+public class TestSale {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
