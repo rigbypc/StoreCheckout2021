eb6392c69dce4e168ae6e36a17fd6c5b7ac13bb5
Peter  C Rigby <rigbypc@gmail.com>
2021-09-21 11:26:07 -0400

    forgot to add hash storage

--- src/point/of/sale/Sale.java
+++ src/point/of/sale/Sale.java
@@ -1,25 +1,33 @@
 package point.of.sale;
 
-public class Sale {
 
+public class Sale {
+	
 	HashStorage hashLookup;
 	ArtR56 artR56;
-
-	public Sale (HashStorage hashLookup) {
-		this.hashLookup = hashLookup;
-		this.artR56 = artR56;
+	
+	public Sale () {
+		
+		//the display terminal is called ArtR56
+		artR56 = new ArtR56();
+		
+		//Storage, add the items in the store
+		hashLookup = new HashStorage();
+		hashLookup.put("1A", "Milk, 3.99");
+		hashLookup.put("2A", "Bread, 4.99");
+		
 	}
-
+	
 	public void scan(String barcode) {
 		//display the barcode
 		artR56.showLine(barcode);
-
+		
 		//lookup barcode in postgres and get item
 		String item = hashLookup.barcode(barcode);
-
+		
 		//display the item
 		artR56.showLine(item);
-
+		
 	}
 
 }
