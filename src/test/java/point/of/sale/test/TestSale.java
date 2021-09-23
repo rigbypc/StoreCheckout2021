54906839bafebe1786d6dbd2bd022975ff1ff512
Peter  C Rigby <rigbypc@gmail.com>
2021-09-23 12:39:10 -0400

    Adding Mock display

--- src/test/java/point/of/sale/test/TestSale.java
+++ src/test/java/point/of/sale/test/TestSale.java
@@ -2,23 +2,28 @@ package point.of.sale.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 
 import point.of.sale.*;
 
 public class TestSale {
 
-	@BeforeEach
-	public void setUp() throws Exception {
-	}
-
 	@Test
-	public void test() {
+	public void testScanFake() {
 		FakeDisplay fakeDisplay = new FakeDisplay();
 		Sale sale = new Sale(fakeDisplay);
 		sale.scan("1A");
 		assertEquals("Milk, 3.99", fakeDisplay.getLastLine());
+	}
+	
+	@Test
+	public void testScanMock() {
+		Display display = mock(Display.class);
+		Sale sale = new Sale(display);
+		sale.scan("1A");
+		verify(display).showLine("Milk, 3.99");
 	}
 
 }
