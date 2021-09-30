2f2ff7763d7d4253ff345147e6dfd986d79de417
Peter  C Rigby <rigbypc@gmail.com>
2021-09-30 12:24:44 -0400

    ArgumentCaptor example

--- src/test/java/point/of/sale/test/TestSale.java
+++ src/test/java/point/of/sale/test/TestSale.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 
 import point.of.sale.*;
@@ -46,10 +47,14 @@ public class TestSale {
 	public void testMockStorage() {
 		Storage storage = mock(Storage.class);
 		Display display = mock(Display.class);
+		ArgumentCaptor<String> barcodeCaptor = 
+				ArgumentCaptor.forClass(String.class);
+		
 		Sale sale = new Sale(display, storage);
-		when(storage.barcode("1A")).thenReturn("Milk, 3.99");
-		sale.scan("1A");
-		verify(display).showLine("1A");
+		when(storage.barcode("2A")).thenReturn("Milk, 3.99");
+		sale.scan("2A");
+		verify(storage).barcode(barcodeCaptor.capture());
+		verify(display).showLine(barcodeCaptor.getValue());
 		verify(display).showLine("Milk, 3.99");
 	}
 
