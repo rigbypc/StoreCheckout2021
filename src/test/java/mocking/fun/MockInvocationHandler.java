459bc1a6720b0ef9c45cbb48f737847711525bf5
Peter  C Rigby <rigbypc@gmail.com>
2021-09-30 13:01:36 -0400

    An example of a proxy with reflection

--- src/test/java/mocking/fun/MockInvocationHandler.java
+++ src/test/java/mocking/fun/MockInvocationHandler.java
@@ -12,7 +12,21 @@ public class MockInvocationHandler implements InvocationHandler {
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		
+		System.out.println(method.getName());
+		
+		for (Object arg : args) {
+			System.out.println(arg.toString());
+			verifyMap.put(method.getName(), arg.toString());
+		}
+		
 		return null;
+	}
+	
+	public void verify(String method_expected, String arg_expected) {
+		//list of arguments for each method, and check that list (exercise)
+		//storing the last interaction with the method
+		assertEquals(arg_expected, verifyMap.get(method_expected));
 	}
 	
 }
--- src/test/java/mocking/fun/TestProxy.java
+++ src/test/java/mocking/fun/TestProxy.java
@@ -15,18 +15,48 @@ import point.of.sale.Storage;
 public class TestProxy {
 
 	@Test
+	public void showMethods() {
+		Method[] methods = Display.class.getDeclaredMethods();
+		
+		for (Method method : methods) {
+			System.out.println(method);	
+		}
+		
+		MockInvocationHandler mockHandler = new MockInvocationHandler();
+		Display display = (Display) Proxy.newProxyInstance(
+				//class
+				Display.class.getClassLoader(), 
+				//methods
+				new Class[] {Display.class}, 
+				mockHandler);
+		
+		display.showLine("1A");
+		mockHandler.verify("showLine", "1A");
+				
+	}
+	
+	@Test
 	public void testProxy() {
 		
 		//do it using mockito
 		Storage storage = mock(Storage.class);
 		
 		//we want to replace the display with our proxy
-		Display display = mock(Display.class);
+		//Display display = mock(Display.class);
+		MockInvocationHandler mockHandler = new MockInvocationHandler();
+		Display display = (Display) Proxy.newProxyInstance(
+				//class
+				Display.class.getClassLoader(), 
+				//methods
+				new Class[] {Display.class}, 
+				mockHandler);
+		
 		Sale sale = new Sale(display, storage);
 		when(storage.barcode("1A")).thenReturn("Milk, 3.99");
 		sale.scan("1A");
-		verify(display).showLine("1A");
-		verify(display).showLine("Milk, 3.99");
+		
+		//verify(display).showLine("Milk, 3.99");
+		mockHandler.verify("showLine", "Milk, 3.99");
 	}
 
 }
