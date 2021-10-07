4cd29c0d68839fc0d3842cbe63ac6c51e0ebf14a
Peter  C Rigby <rigbypc@gmail.com>
2021-10-07 11:20:57 -0400

    Make a main class that can read from the console

---
+++ src/main/java/point/of/sale/ConsoleSale.java
@@ -0,0 +1,33 @@
+package point.of.sale;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleSale {
+
+	public static void main(String[] args) throws IOException {
+		
+		 // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+ 
+ 
+		
+		while(true) {
+	        // Printing the read line
+			
+	        System.out.println("Type in item barcode:");
+			
+			// Reading data using readLine
+	        String line = reader.readLine();
+	 	        
+	        if (line.equals("Exit")) {
+	        	break;
+	        }
+	       
+		}
+
+	}
+
+}
