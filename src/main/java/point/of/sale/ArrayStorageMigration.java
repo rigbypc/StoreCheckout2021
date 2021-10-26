5c5c156d80489fcf9656b63fb6e55c1faaee5fa8
Peter  C Rigby <rigbypc@gmail.com>
2021-10-26 12:30:39 -0400

    Extend hashstorage, for migration

---
+++ src/main/java/point/of/sale/ArrayStorageMigration.java
@@ -0,0 +1,21 @@
+package point.of.sale;
+
+public class ArrayStorageMigration extends HashStorage {
+
+	public ArrayStorageMigration() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void put(String barcode, String item) {
+		// TODO Auto-generated method stub
+		super.put(barcode, item);
+	}
+
+	@Override
+	public String barcode(String barcode) {
+		// TODO Auto-generated method stub
+		return super.barcode(barcode);
+	}
+
+}
--- src/test/java/point/of/sale/test/TestDataMigration.java
+++ src/test/java/point/of/sale/test/TestDataMigration.java
@@ -13,9 +13,20 @@ class TestDataMigration {
 	void test() {
 		IDisplay display = mock(IDisplay.class);
 		
-		IStorage storage = new HashStorage();
+		IStorage storage = new ArrayStorageMigration();
 		storage.put("1", "Milk, 3.99");
 		storage.put("2", "Beer, 3.99");
+		
+		//Forklift (mass migration)
+
+		//Consistency Checking (includes incremental replication)
+
+		//Shadow writes (writes to old and new datastore)
+
+		//Shadow reads (checks that old and new datastores return the same value
+
+		//Read and write from new datastore (get rid of old datastore)
+
 		
 		Sale sale = new Sale(display, storage);
 		
